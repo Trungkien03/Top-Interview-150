0bae4bf8c3dee223256d71125056bd148164a2c9
Trung Kiên Nguyễn <trungkiennguyen0310@gmail.com>
2024-12-21 23:37:18 +0700

    find divisors

---
+++ src/FindDivisors/index.js
@@ -0,0 +1,14 @@
+"use strict";
+function findDivisors(n) {
+    const divisors = [];
+    for (let i = 1; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            divisors.push(i); // Thêm ước số nhỏ
+            if (i !== n / i) {
+                divisors.push(n / i); // Thêm ước số lớn
+            }
+        }
+    }
+    return divisors.sort((a, b) => a - b); // Sắp xếp theo thứ tự tăng dần
+}
+console.log(findDivisors(12)); // Output: [1, 2, 3, 4, 6, 12]

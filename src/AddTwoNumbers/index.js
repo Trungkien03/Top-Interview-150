21b1bfb2ed0067875818764b7db59514e9a20f99
Trung Kiên Nguyễn <trungkiennguyen0310@gmail.com>
2024-12-21 23:28:02 +0700

    check isPrime and addTwoNumbers

---
+++ src/AddTwoNumbers/index.js
@@ -0,0 +1,29 @@
+"use strict";
+class ListNode {
+    constructor(val = 0, next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+function addTwoNumbers(l1, l2) {
+    let dummy = new ListNode();
+    let res = dummy;
+    let total = 0, carry = 0;
+    // Lặp qua khi còn phần tử trong l1, l2 hoặc carry
+    while (l1 || l2 || carry) {
+        total = carry; // Khởi tạo tổng bằng giá trị "nhớ" từ phép cộng trước đó
+        if (l1) {
+            total += l1.val; // Cộng giá trị của l1
+            l1 = l1.next; // Chuyển đến nút tiếp theo
+        }
+        if (l2) {
+            total += l2.val; // Cộng giá trị của l2
+            l2 = l2.next; // Chuyển đến nút tiếp theo
+        }
+        let num = total % 10; // Giá trị của nút hiện tại
+        carry = Math.floor(total / 10); // Tính giá trị "nhớ" cho nút tiếp theo
+        dummy.next = new ListNode(num); // Tạo nút mới trong danh sách kết quả
+        dummy = dummy.next; // Di chuyển con trỏ `dummy` đến nút vừa thêm
+    }
+    return res.next; // Trả về danh sách kết quả, bỏ qua nút giả
+}
---
+++ src/IsPrime/index.js
@@ -0,0 +1,10 @@
+"use strict";
+function isPrime(n) {
+    if (n < 2)
+        return false;
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0)
+            return false;
+    }
+    return true;
+}
